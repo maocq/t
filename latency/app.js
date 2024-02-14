d6103e8148926d63ceb02f96fef93b97d3cdcd8d
maocq <johncarm@bancolombia.com.co>
2024-02-14 14:03:53 -0500

    Test

--- latency/app.js
+++ latency/app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 8080
 
 app.get('/:latency?', (req, res) => {
   setTimeout(() => res.json({id: "CC12345" + Math.floor(Math.random() * 9999), name: "Foo"}), req.params.latency || 0);
