33f072b823fab06218e01e85a6bff2145b049e3d
maocq <johncarm@bancolombia.com.co>
2024-02-14 14:25:29 -0500

    Test

---
+++ test/performance-analyzer/case-three.exs
@@ -0,0 +1,21 @@
+import Config
+
+config :distributed_performance_analyzer,
+  url: "http://_IP_:8080/api/case-three",
+  request: %{
+    method: "GET",
+    headers: [],
+    body: ""
+  },
+  execution: %{
+    steps: 20,
+    increment: 50,
+    duration: 500,
+    constant_load: false,
+    dataset: :none,
+    separator: ","
+  },
+  distributed: :none
+
+config :logger,
+  level: :warn
