59a95575bf480640a85b51347e6990f2eec4f821
maocq <johncarm@bancolombia.com.co>
2024-04-09 22:13:12 -0500

    t

--- spring/src/main/java/com/maocq/reactive/infrastructure/drivenadapter/database/config/PostgreSQLConnectionPool.java
+++ spring/src/main/java/com/maocq/reactive/infrastructure/drivenadapter/database/config/PostgreSQLConnectionPool.java
@@ -12,7 +12,7 @@ import java.time.Duration;
 @Configuration
 public class PostgreSQLConnectionPool {
     public static final int INITIAL_SIZE = 10;
-    public static final int MAX_SIZE = 10;
+    public static final int MAX_SIZE = 20;
     public static final int MAX_IDLE_TIME = 30;
 
     @Bean
